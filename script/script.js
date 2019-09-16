149979ebaf07138d35a88cef93884e5b3de292d9
Andrey Zhukov <zhukov290791@gmail.com>
2019-09-16 14:34:13 +0500

    third commit

--- script/script.js
+++ script/script.js
@@ -1,31 +1,40 @@
-let start = document.querySelector('.buttonOne'),
-    selectorIntro = document.querySelector('.intro'),
-    selectorOutro = document.querySelector('.outro');
+let selector = prompt('введите селектор начиная с точки или #'),
+    height = prompt('введите требуемую высоту height, в пикселях'),
+    width = prompt('введите требуемую ширину width, в пикселях'),
+    bg = prompt('Введите требуемый цвет фона'),
+    fontsize = prompt('введите размер шрифта, в пикселях');
 
-
-
-
-const DomElement = function () {
-    this.bg = 'background-color: gray; ';
-    this.height = 'height: 100px; ';
-    this.width = 'width: 100px; ';
-    this.fontSize = 'font-size: 30px; ';
+function DomElement () {
+    this.selector = selector;
+    this.bg = bg;
+    this.height = height;
+    this.width = width;
+    this.fontSize = fontsize;
 };
-
-DomElement.prototype.func = function () {
-    let temp = null,
-        typeDivOrP = null;
-    if (selectorIntro.value[0] === '.') {
-        typeDivOrP = 'p';
-    } else if (selectorIntro.value[0] === '#') {
-        typeDivOrP = 'div';
-    } else return;
-    selectorOutro.style.cssText = this.bg + this.height + this.width + this.fontSize;
-    temp = '<' + typeDivOrP + ' class="outro">' + _this.selectorIntro.value + '</' + typeDivOrP + '>';
-    selectorOutro.innerHTML = temp;
+//creareelement p или div
+DomElement.prototype.createDivOrP = function () {
+    let divOrP = null;
+    if (this.selector.charAt(0) == '.') {
+        divOrP = document.createElement('div');
+        divOrP.className = this.selector;
+        divOrP.innerHTML = 'Элемент Div';
+        divOrP.style.cssText = 'height:' + this.height + 'px; width:' + this.width + 'px; background:' + this.bg + '; font-size: ' + this.fontSize + 'px;';
+        document.body.appendChild(divOrP);
+    }
+    else if (this.selector.charAt(0) == '#') {
+        divOrP = document.createElement('p');
+        divOrP.id = this.selector;
+        divOrP.innerHTML = 'Элемент P';
+        divOrP.style.cssText = 'height:' + this.height + 'px; width:' + this.width + 'px; background:' + this.bg + '; font-size: ' + this.fontSize + 'px;';
+        document.body.appendChild(divOrP);
+    }
+    else {
+        document.write('Неверно передан селектор');
+    }
 };
 
 const domElement = new DomElement();
 
-start.addEventListener('click', domElement.func.bind(domElement));
+domElement.createDivOrP();
+
 
